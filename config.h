0757ea3d179226e3dcff739930ce6b21b142c7c2
dwdwdan <dan.walters5@outlook.com>
2021-08-17 14:51:42 +0100

    Initial commit for fork

--- config.h
+++ config.h
@@ -5,22 +5,22 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Jetbrainsmono:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#f8f8f2", "#282a36" },
 	[SchemeSel] = { "#f8f8f2", "#44475a" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+	[SchemeSelHighlight] = { "#50fa7b", "#44475a" },
+	[SchemeNormHighlight] = { "#50fa7b", "#282a36" },
+	[SchemeOut] = { "#bd93f9", "#44475a" },
+	[SchemeOutHighlight] = { "#bf93f9", "#44475a" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lineheight = 32;
 static unsigned int min_lineheight = 8;
 
 /*
